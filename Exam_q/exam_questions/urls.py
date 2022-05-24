127de3f49a3065610cc12d83022536532625c8cd
Илья Прокофьев <i.prokofev@g.nsu.ru>
2022-05-24 16:34:01 +0700

    First working version

--- Exam_q/exam_questions/urls.py
+++ Exam_q/exam_questions/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:filename>', views.params, name='params')
+    path('<str:filename>', views.params, name='params'),
+    path('preview/downloadfile/<str:filename>', views.downloadfile, name='downloadfile'),
+    path('preview/<str:filename>', views.preview, name='preview'),
 ]
--- Exam_q/exam_questions/views.py
+++ Exam_q/exam_questions/views.py
@@ -1,14 +1,60 @@
-from django.http import HttpResponse
 from django.http import HttpResponseRedirect
+from django.http.response import HttpResponse
 from django.shortcuts import render
 from .forms import UploadFileForm, UploadParamsForm
-from django.urls import path
+import os
+import random
+import mimetypes
+from wsgiref.util import FileWrapper
 
 def handle_image(f):
     with open('exam_questions/static/upload/'+f.name, 'wb+') as destination:
         print(type(f.name))
         for chunk in f.chunks():
             destination.write(chunk)
+
+
+def create_pdf(folder, name, params):
+    os.chdir(folder)
+    title = []
+    with open(name, encoding='UTF-8') as f:
+        all_questions = f.readlines()
+
+    idx = 0
+    while 'begin{document}' not in all_questions[idx]:
+        title.append(all_questions[idx])
+        idx += 1
+
+    questions_pool = {3: [], 4: [], 5: []}
+    for line in all_questions:
+        if params['label_3'] in line:
+            questions_pool[3].append(line)
+        elif params['label_4'] in line:
+            questions_pool[4].append(line)
+        elif params['label_5'] in line:
+            questions_pool[5].append(line)
+
+    with open('tickets.tex', 'w') as f:
+        for line in title:
+            f.write('%s\n' % line)
+        f.write('\\begin{document} \n')
+        for ticket in range(params['number_of_tickets']):
+            f.write('\\begin{center} {\\Large Билет №%s} \\end{center} \n' % str(ticket + 1))
+            questions = []
+            points = [3, 4, 5]
+            for point in points:
+                for question in range(params[point]):
+                    choice = random.choice(questions_pool[point])
+                    while choice in questions:
+                        choice = random.choice(questions_pool[point])
+                    questions.append(random.choice(questions_pool[point]))
+            f.write('\n')
+            for line in questions:
+                f.write('%s\n' % line)
+
+        f.write('\\end{document}')
+    os.system(f"pdflatex tickets.tex")
+    os.chdir("../../..")
 
 
 def index(request):
@@ -25,8 +71,30 @@ def params(request, filename):
     if request.method == 'POST':
         form = UploadParamsForm(request.POST, request.FILES)
         if form.is_valid():
-            # handle_uploaded_file(request.FILES['file'])
-            return HttpResponseRedirect(f"/exam_questions/{filename}/success")
+            create_pdf("exam_questions/static/upload/", filename, {'label_3': '3.png', 'label_4': '4.png',
+                                                                   'label_5': '5.png',
+                                                                   'number_of_tickets': form.cleaned_data['num_tickets'],
+                                                                    3: form.cleaned_data['num_questions_3_in_ticket'],
+                                                                    4: form.cleaned_data['num_questions_4_in_ticket'],
+                                                                    5: form.cleaned_data['num_questions_5_in_ticket']})
+            return HttpResponseRedirect(f"/exam_questions/preview/tickets.pdf")
     else:
         form = UploadParamsForm()
     return render(request, 'params.html', {'form': form})
+
+def downloadfile(request, filename):
+    if filename != '':
+        path = open(f'exam_questions/static/upload/{filename}', 'rb')
+        mime_type, _ = mimetypes.guess_type(f'/exam_questions/static/upload/{filename}')
+        response = HttpResponse(path, content_type='pdf')
+        response['Content-Disposition'] = "attachment; filename=%s" % filename
+        return response
+    else:
+        return HttpResponse("No such file")
+
+def preview(request, filename):
+    # if request.method == 'POST':
+
+    return render(request, 'preview.html', {'filename': filename})
+
+
