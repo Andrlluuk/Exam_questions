fca32fa07e73643c08c18a8942ff959146c70e28
andrlluuk <andrlluuk@gmail.com>
2022-05-25 12:08:26 +0700

    change html, add css, fonts, add labels to Django.Form

--- Exam_q/exam_questions/forms.py
+++ Exam_q/exam_questions/forms.py
@@ -1,15 +1,15 @@
 from django import forms
 
 class UploadFileForm(forms.Form):
-    file = forms.FileField()
+    file = forms.FileField(label='Программа')
 
 class UploadParamsForm(forms.Form):
-    num_tickets = forms.IntegerField()
+    num_tickets = forms.IntegerField(label='Количество билетов')
     CHOICES = (
         ('PDF', '.pdf'),
         ('TEX', '.tex'),
     )
-    num_questions_3_in_ticket = forms.IntegerField()
-    num_questions_4_in_ticket = forms.IntegerField()
-    num_questions_5_in_ticket = forms.IntegerField()
-    output_format = forms.ChoiceField(choices=CHOICES)
+    num_questions_3_in_ticket = forms.IntegerField(label='Количество вопросов на 3')
+    num_questions_4_in_ticket = forms.IntegerField(label='Количество вопросов на 4')
+    num_questions_5_in_ticket = forms.IntegerField(label='Количество вопросов на 5')
+    output_format = forms.ChoiceField(choices=CHOICES, label='Выходной формат файла')
