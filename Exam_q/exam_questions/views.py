22f44c5517ca754daef7190913d480e75253ef91
andrlluuk <andrlluuk@gmail.com>
2022-05-27 15:51:22 +0700

    Add function to make non-separable questions

--- Exam_q/exam_questions/views.py
+++ Exam_q/exam_questions/views.py
@@ -5,10 +5,9 @@ from .forms import UploadFileForm, UploadParamsForm
 import os
 import random
 import mimetypes
-import tex2pix
-from wsgiref.util import FileWrapper
-import sympy
-from pnglatex import pnglatex
+from pdf2image import convert_from_path
+from fpdf import FPDF
+from PIL import Image
 
 def handle_image(f):
     with open('exam_questions/static/upload/'+f.name, 'wb+') as destination:
@@ -61,7 +60,7 @@ def create_pdf(folder, name, params):
     os.system(f"pdflatex tickets.tex")
     os.chdir("../../..")
 
-"""def create_pages(folder, name, params):
+def create_pages(folder, name, params):
     os.chdir(folder)
     title = []
     with open(name, encoding='UTF-8') as f:
@@ -69,6 +68,9 @@ def create_pdf(folder, name, params):
 
     idx = 0
     while 'begin{document}' not in all_questions[idx]:
+        if ('documentclass' in all_questions[idx]) or ('documentarticle' in all_questions[idx]) :
+            idx += 1
+            continue
         title.append(all_questions[idx])
         idx += 1
 
@@ -82,32 +84,48 @@ def create_pdf(folder, name, params):
         elif params['label_5'] in line:
             questions_pool[5].append(line)
 
-    questions = []
-    points = [3, 4, 5]
-    for point in points:
-        for question in range(params[point]):
-            choice = random.choice(questions_pool[point])
-            while choice in questions:
-                choice = random.choice(questions_pool[point])
-            questions.append(choice)
     for ticket in range(params['number_of_tickets']):
-        with open(f"tickets{ticket}.tex", 'w') as f:
+        questions = []
+        points = [3, 4, 5]
+        for point in points:
+            for question in range(params[point]):
+                choice = random.choice(questions_pool[point])
+                while choice in questions:
+                    choice = random.choice(questions_pool[point])
+                questions.append(choice)
+        with open(f"tickets{ticket + 1}.tex", 'w') as f:
+            f.write('\\documentclass[preview]{standalone} \n')
             for line in title:
                 f.write('%s\n' % line)
             f.write('\\begin{document} \n')
-            for ticket in range(params['number_of_tickets']):
-                f.write('\\begin{center} {\\Large Билет №%s} \\end{center} \n' % str(ticket + 1))
+            f.write('\\begin{center} {\\Large Билет №%s} \\end{center} \n' % str(ticket + 1))
             f.write('\n')
             for line in questions:
                 f.write('%s\n' % line)
             f.write('\\end{document}')
-        f = open(f"tickets{ticket}.tex")
-        r = tex2pix.Renderer(f, runbibtex=True, extras=['example.bib'])
-        # r.verbose = True # be loud to the terminal
-        # r.rmtmpdir = False # keep the working dir around, for debugging
-        r.mkpng(f'example{ticket}.png')
 
-    create_pdf(folder, name, params)"""
+        os.system(f"pdflatex tickets{ticket + 1}.tex")
+
+    for ticket in range(params['number_of_tickets']):
+        file = convert_from_path(f'tickets{ticket + 1}.pdf', 500)
+        for fil in file:
+            fil.save(f'tickets{ticket + 1}.png', 'PNG')
+
+    pdf = FPDF()
+    x_current = 20
+    y_current = 20
+    pdf.add_page()
+    for image in [f'tickets{ticket + 1}.png' for ticket in range(params['number_of_tickets'])]:
+        im = Image.open(image)
+        width, height = im.size
+        coef = width/160
+        if (y_current + height/coef > 280):
+            y_current = 20
+            pdf.add_page()
+        pdf.image(image, x_current, y_current, width/coef, height/coef)
+        y_current += height/coef + 5
+    pdf.output("tickets.pdf", "F")
+
 
 
 def index(request):
@@ -124,7 +142,7 @@ def params(request, filename):
     if request.method == 'POST':
         form = UploadParamsForm(request.POST, request.FILES)
         if form.is_valid():
-            create_pdf("exam_questions/static/upload/", filename, {'label_3': '3.png', 'label_4': '4.png',
+            create_pages("exam_questions/static/upload/", filename, {'label_3': '3.png', 'label_4': '4.png',
                                                                    'label_5': '5.png',
                                                                    'number_of_tickets': form.cleaned_data['num_tickets'],
                                                                     3: form.cleaned_data['num_questions_3_in_ticket'],
